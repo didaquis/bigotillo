f262ffbed7ee5b49eeefe76dd335b07faddaa2b3
Dídac García <didaquis@gmail.com>
2019-11-02 22:46:42 +0100

    Improved some things

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
 const { bold, yellow, bgBlack } = require('colorette');
 
-const newline = '\n'
+const newline = '\n';
 const bigotilloText = `${yellow(bold(bgBlack(' Bigotillo   {   } ')))}`;
 
 const output = newline + bigotilloText + newline;
 
-console.log(output);
+console.log(output); // eslint-disable-line no-console
