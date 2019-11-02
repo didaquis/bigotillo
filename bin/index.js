f5146fdb4373e1e168f3ef6b8ce31600428a965d
Dídac García <didaquis@gmail.com>
2019-11-02 22:41:32 +0100

    New version

---
+++ bin/index.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+const { bold, yellow, bgBlack } = require('colorette');
+
+const newline = '\n'
+const bigotilloText = `${yellow(bold(bgBlack(' Bigotillo   {   } ')))}`;
+
+const output = newline + bigotilloText + newline;
+
+console.log(output);
